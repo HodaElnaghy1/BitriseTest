3e7a1d8b90f97e8e3553dd67b87fa9481920d5de
Hoda Elnaghy <hoda.elnaghy@souhoola.com>
2024-07-03 11:22:30 +0300

    initial commit

---
+++ BitriseTest/BitriseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseTestApp.swift
+//  BitriseTest
+//
+//  Created by Hoda Elnaghy on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BitriseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
